ffd5c028f29d93688b03dd9b6d56c8cbd379ee62
franciscocontreras-crypto <francisco.contreras@uabc.edu.mx>
2026-03-11 20:57:02 -0700

    actividad funcion para calcular el radio

--- unit_1/practica_3.scala
+++ unit_1/practica_3.scala
@@ -1,6 +1,9 @@
 // Practica 3
 
 // 1. Función para calcular el radio de un círculo a partir del área
+def calcularRadio(area: Double): Double = {
+  math.sqrt(area / math.Pi)
+}
 
 // 2. Función para verificar si un número es par
 
