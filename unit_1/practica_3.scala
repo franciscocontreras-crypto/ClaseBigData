e0e0811ff45dfa103471c6191176f7fe02690c97
franciscocontreras-crypto <francisco.contreras@uabc.edu.mx>
2026-03-11 20:30:41 -0700

    practica_3.scala

---
+++ unit_1/practica_3.scala
@@ -0,0 +1,9 @@
+// Practica 3
+
+// 1. Función para calcular el radio de un círculo a partir del área
+
+// 2. Función para verificar si un número es par
+
+// 3. Ejercicios de strings
+
+// 4. Ejercicios de tuplas
